7bac641743e703545ec06749754f4890aed493b8
vikramkumar80 <49632493+vikramkumar80@users.noreply.github.com>
2021-01-22 16:54:50 +0530

    Add files via upload

---
+++ login_page/index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOGIN</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<form action="" method="post">
+		<h2>LOGIN</h2>
+		<label>User Name</label>
+		<input type="text" name="username" placeholder="User Name">
+
+		<label>Password</label>
+		<input type="password" name="password" placeholder="Password"><br>
+
+		<button type="submit" >Login</button>
+
+	</form>
+</body>
+</html>
